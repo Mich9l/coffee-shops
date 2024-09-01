25ac22a3ecf83c697d0a8f520f7bf79e2fa6310f
Michail Kuzmin <Mich9l@yandex.ru>
2024-09-01 20:20:20 +0300

    therd commit

--- src/btnAllproduct.js
+++ src/btnAllproduct.js
@@ -5,8 +5,10 @@ const div = document.getElementById('coffee-salary-section__wrap-hide')
 function toggleDivVisibility(){
     if (div.style.display ==="none"){
         div.style.display = "block"
+        btnImage.innerText = "All products"
     }else {
         div.style.display = "none"
+        btnImage.innerText = "View all products"
     }
 }
 
